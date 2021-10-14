9f805695e77eaea3a860d8fc75ac7f4ba1939109
jl2237 <jl2237@njit.edu>
2021-10-14 13:14:30 -0400

    Updating in case

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,5 +1,5 @@
 <?php
-require(__DIR__."/../../partials/nav.php");?>
+require(__DIR__ . "/../../partials/nav.php"); ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
@@ -20,49 +20,74 @@ require(__DIR__."/../../partials/nav.php");?>
     }
 </script>
 <?php
- //TODO 2: add PHP Code
- if(isset($_POST["email"]) && isset($_POST["password"])){
-     //get the email key from $_POST, default to "" if not set, and return the value
-     $email = se($_POST, "email","", false);
-     //same as above but for password
-     $password = se($_POST, "password", "", false);
-     //TODO 3: validate/use
-     
-     
-     //sanitize
-     $email = sanitize_email($email);
-     if(count($errors) > 0){
-         flash(var_export($errors, true));
-     }
-     else{
-         //TODO 4
-         $db= getDB();
-         $stmt = $db->prepare("SELECT email, password FROM Users WHERE email = :email");
-         try{
-             $r =$stmt->execute([":email" => $email]);
-             if($r){
-                 $user = $stmt->fetch(PDO::FETCH_ASSOC);
-                 if($user){
-                     $hash = $user["password"];
-                     unset($user["password"]);
-                     if(password_verify($password, $hash)){
-                         flash("Welcome, $email");
-                         $_SESSION["user"] = $user;
-                         die(header("Location: home.php"));
-                     }
-                     else
-                     {
-                         flash("Password doesn't match");
-                     }
-                 }else{
-                     flash("Invalid email");
-                 }
-             }
-         }catch(Exception $e){
-             flash(var_export($e, true));
-         }
-     }
- }
+//TODO 2: add PHP Code
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    //get the email key from $_POST, default to "" if not set, and return the value
+    $email = se($_POST, "email", "", false);
+    //same as above but for password
+    $password = se($_POST, "password", "", false);
+    //TODO 3: validate/use
+    //$errors = [];
+    $hasErrors = false;
+    if (empty($email)) {
+        //array_push($errors, "Email must be set");
+        flash("Email must be set", "warning");
+        $hasErrors = true;
+    }
+    //sanitize
+    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = sanitize_email($email);
+    //validate
+    //if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!is_valid_email($email)) {
+        //array_push($errors, "Invalid email address");
+        flash("Invalid email address", "warning");
+        $hasErrors = true;
+    }
+    if (empty($password)) {
+        //array_push($errors, "Password must be set");
+        flash("Password must be set");
+        $hasErrors = true;
+    }
+    if (strlen($password) < 8) {
+        //array_push($errors, "Password must be 8 or more characters");
+        flash("Password must be at least 8 characters", "warning");
+        $hasErrors = true;
+    }
+    if ($hasErrors) {
+        //Nothing to output here, flash will do it
+        //can likely flip the if condition
+        //echo "<pre>" . var_export($errors, true) . "</pre>";
+    } else {
+        //TODO 4
+        $db = getDB();
+        $stmt = $db->prepare("SELECT id, username, email, password from Users where email = :email");
+        try {
+            $r = $stmt->execute([":email" => $email]);
+            if ($r) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    $hash = $user["password"];
+                    unset($user["password"]);
+                    if (password_verify($password, $hash)) {
+                        ///echo "Weclome $email";
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
+                    } else {
+                        //echo "Invalid password";
+                        flash("Invalid password", "danger");
+                    }
+                } else {
+                    //echo "Invalid email";
+                    flash("Email not found", "danger");
+                }
+            }
+        } catch (Exception $e) {
+            //echo "<pre>" . var_export($e, true) . "</pre>";
+            flash(var_export($e, true));
+        }
+    }
+}
 ?>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
