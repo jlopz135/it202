d53757590fc134ebbeeef69db64117dcbd80c69e
jl2237 <jl2237@njit.edu>
2021-12-13 19:09:48 -0500

    Users table is there, need to fix public stuff

---
+++ public_html/Project/showUsers.php
@@ -0,0 +1,48 @@
+<?php
+require(__DIR__ . "/../../partials/nav.php");
+//all orders can be displayed for admin and owner on this page
+if (!has_role("Admin") && !has_role("Owner")) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: $BASE_PATH" . "home.php"));
+}
+
+$db = getDB();
+//generally try to avoid SELECT *, but this is about being dynamic so I'm using it this time
+$query = "SELECT * FROM Users"; //TODO change table name and desired columns
+$stmt = $db->prepare($query);
+$results = [];
+try {
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "<pre>" . var_export($e, true) . "</pre>";
+}
+?>
+
+
+
+<h3>All Orders</h3>
+<?php if ($results == 0) : ?>
+    <p>No results to show</p>
+<?php else : ?>
+    <table padding=5 style="text-align:left;">
+        
+            <tr><h3>Usernames:</h3>
+            <?php foreach ($results as $result) : ?>
+                <td>
+                    <a href="profile.php?id=<?php se($result, 'user_id'); ?>"><?php se($result, "username"); ?></a>
+                    <?php $user_id = se($result, "user_id", 0, false);
+                    $username = se($result, "username", "", false);
+                    ?>
+                </td>
+                <td><?php se($result, "created"); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+
+<?php endif; ?>
+
+<?php
+require_once(__DIR__ . "/../../partials/footer.php");
+?>
--- public_html/Project/userProfile.php
+++ public_html/Project/userProfile.php
@@ -112,6 +112,7 @@ $public = false;
 //TODO pull any other public info you want
 $db = getDB();
 $stmt = $db->prepare("SELECT username, created, visibility from Users where id = :id");
+$results = [];
 try {
     $stmt->execute([":id" => $user_id]);
     $r = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -124,13 +125,109 @@ try {
         redirect("home.php");
         //die(header("Location: home.php"));
     }
+    $x = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($x) {
+        $results = $x;
+    }
 } catch (Exception $e) {
     echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
 }
 ?>
+<div class="container-fluid">
+    <h1>Profile</h1> <table>
 
-<?php if (!$edit) : ?>
-    <div>Username: <?php se($username); ?></div>
-    <div>Joined: <?php se($created); ?></div>
-    <!-- TODO any other public info -->
-<?php endif; ?>
+    <?php if ($isMe) : ?>
+        <?php if ($edit) : ?>
+           
+            <a class="btn btn-primary" href="?">View</a>
+        <?php else : ?>
+            <a class="btn btn-primary" href="?edit=true">Edit</a>
+        <?php endif; ?>
+    <?php endif; ?>
+    <div>
+    </div>
+    <div>
+        <table class="table text-light">
+
+            <tbody>
+                <?php foreach ($results as $item) : ?>
+                    <tr>
+                        <td><?php se($item, "username"); ?></td>
+
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <?php if (!$edit) : ?>
+        <div>Username: <?php se($username); ?></div>
+        <div>Joined: <?php se($created); ?></div>
+        <!-- TODO any other public info -->
+    <?php endif; ?>
+
+    <?php if ($isMe && $edit) : ?>
+        <form method="POST" onsubmit="return validate(this);">
+            <div class="mb-3">
+                <div class="form-check form-switch">
+                    <input name="visibility" class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" <?php if ($public) echo "checked"; ?>>
+                    <label class="form-check-label" for="flexSwitchCheckDefault">Make Profile Public</label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="email">Email</label>
+                <input class="form-control" type="email" name="email" id="email" value="<?php se($email); ?>" />
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="username">Username</label>
+                <input class="form-control" type="text" name="username" id="username" value="<?php se($username); ?>" />
+            </div>
+            <!-- DO NOT PRELOAD PASSWORD -->
+            <div class="mb-3">Password Reset</div>
+            <div class="mb-3">
+                <label class="form-label" for="cp">Current Password</label>
+                <input class="form-control" type="password" name="currentPassword" id="cp" />
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="np">New Password</label>
+                <input class="form-control" type="password" name="newPassword" id="np" />
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="conp">Confirm Password</label>
+                <input class="form-control" type="password" name="confirmPassword" id="conp" />
+            </div>
+            <input type="submit" class="mt-3 btn btn-primary" value="Update Profile" name="save" />
+        </form>
+    <?php endif; ?>
+</div>
+<script>
+    function validate(form) {
+        let pw = form.newPassword.value;
+        let con = form.confirmPassword.value;
+        let isValid = true;
+        //TODO add other client side validation....
+
+        //example of using flash via javascript
+        //find the flash container, create a new element, appendChild
+        if (pw !== con) {
+            //find the container
+            /*let flash = document.getElementById("flash");
+            //create a div (or whatever wrapper we want)
+            let outerDiv = document.createElement("div");
+            outerDiv.className = "row justify-content-center";
+            let innerDiv = document.createElement("div");
+            //apply the CSS (these are bootstrap classes which we'll learn later)
+            innerDiv.className = "alert alert-warning";
+            //set the content
+            innerDiv.innerText = "Password and Confirm password must match";
+            outerDiv.appendChild(innerDiv);
+            //add the element to the DOM (if we don't it merely exists in memory)
+            flash.appendChild(outerDiv);*/
+            flash("Password and Confirm password must match", "warning");
+            isValid = false;
+        }
+        return isValid;
+    }
+</script>
+<?php
+require_once(__DIR__ . "/../../partials/footer.php");
+?>
